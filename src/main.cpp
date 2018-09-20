02df7566c34715f34e6623b18c8b2eb659ae4c68
Alexey <stalix@mail.ru>
2018-09-20 18:10:04 +0300

    OTA and HttpServerUpdateServer ready.

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ void setup() {
     wifiManager.autoConnect(host,"9268168144");
 
     //if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
+    Serial.println("WiFi connected... :)");
     delay(100);
 
     MDNS.begin(host);
@@ -26,37 +26,49 @@ void setup() {
     httpServer.begin();
     MDNS.addService("http", "tcp", 80);
     Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
+    // Port defaults to 8266
+    // ArduinoOTA.setPort(8266);
 
-    // ArduinoOTA.onStart([]() {
-    //   String type;
-    //   if (ArduinoOTA.getCommand() == U_FLASH)
-    //     type = "sketch";
-    //   else // U_SPIFFS
-    //     type = "filesystem";
-    //
-    //   // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    //   Serial.println("Start updating " + type);
-    // });
-    // ArduinoOTA.onEnd([]() {
-    //   Serial.println("\nEnd");
-    // });
-    // ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    //   Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    // });
-    // ArduinoOTA.onError([](ota_error_t error) {
-    //   Serial.printf("Error[%u]: ", error);
-    //   if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    //   else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    //   else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    //   else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    //   else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    // });
-    // ArduinoOTA.begin();
-    Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
-    Serial.printf("Arduino OTA started. Host : %s\n",host);
+    // Hostname defaults to esp8266-[ChipID]
+    // ArduinoOTA.setHostname("myesp8266");
+
+    // No authentication by default
+    // ArduinoOTA.setPassword("admin");
+
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+    ArduinoOTA.onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+      Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
+    Serial.print("Arduino OTA started. IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop(void){
   httpServer.handleClient();
-  // ArduinoOTA.handle();
+  ArduinoOTA.handle();
 }
